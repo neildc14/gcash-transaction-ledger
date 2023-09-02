7100d0f59b07867739dfd92b92315dcd2dd1830e
neildc14 <genoznightmare@gmail.com>
2023-09-02 11:16:30 +0800

    proptypes modal

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,33 +1,50 @@
-const Modal = ({ isOpen, onClose, children }) => {
-  if (!isOpen) return null;
+import { motion, AnimatePresence } from "framer-motion";
+import PropTypes from "prop-types";
 
+const Modal = ({ isOpen, onClose, children }) => {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center">
-      <div className="fixed inset-0 bg-gray-800 opacity-50"></div>
-      <div className="relative z-10 bg-white p-6 rounded-md shadow-md w-4/5 md:w-96">
-        {children}
-        <button
-          className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
-          onClick={onClose}
+    <AnimatePresence>
+      {isOpen && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
-        </button>
-      </div>
-    </div>
+          <div className="fixed inset-0 z-50 flex items-center justify-center">
+            <div className="fixed inset-0 bg-gray-800 opacity-50"></div>
+            <div className="relative z-10 bg-white p-6 rounded-md shadow-md w-4/5 md:w-96">
+              {children}
+              <button
+                className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+                onClick={onClose}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
+};
+
+Modal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  children: PropTypes.any,
 };
 
 export default Modal;
